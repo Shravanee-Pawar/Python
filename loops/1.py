7317f06e7b857aab828d2de713bf8f88cb64f1ca
Shravanee-Pawar <shravaneepawar.06@gmail.com>
2026-07-29 23:52:36 +0530

    Added latest code updates

---
+++ loops/1.py
@@ -0,0 +1,20 @@
+i = 1
+while(i<51):
+    print(i)
+    i +=1 # i = i+1
+    
+l = [1, "harry",False, "this","rohan","shubham","shubhi"]
+i = 0
+while(i<len(l)):
+    print(l[i])
+    i +=1   
+    
+for j in range(4):
+    print(j)   
+     
+f = [1,4,6,234,6,764]   
+for i in f:
+    print (i) 
+h =(6,231,75,122)
+for i in h:
+    print(i)      
