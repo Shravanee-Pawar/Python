40de84d910fea47324325cfb820a222210db58d3
Shravanee  Chandrashekhrar Pawar <shravaneepawar.06@gmail.com>
2026-09-14 20:44:18 +0530

    Create mobile.py

---
+++ OOP/mobile.py
@@ -0,0 +1,12 @@
+class Mobile:
+    def __init__(self, brand, price):
+        self.brand = brand
+        self.price = price
+
+    def display(self):
+        print("Brand:", self.brand)
+        print("Price:", self.price)
+
+
+m = Mobile("Samsung", 25000)
+m.display()
